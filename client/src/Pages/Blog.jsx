3710562df11e34db00888d19189907bcf4647076
Alicja Bieryt <alicjabieryt@gmail.com>
2024-05-26 14:37:38 +0200

    Compressed images and skeleton loading

--- client/src/Pages/Blog.jsx
+++ client/src/Pages/Blog.jsx
@@ -1,16 +1,43 @@
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { Link } from "react-router-dom";
 import useFetch from "../Hooks/useFetch";
-import "./css/Blog.css";
+import NotFound from "../Pages/NotFound.jsx";
 import config from "../config.js";
+import "./css/Blog.css";
 
 const Blog = () => {
   let { loading, error, blogs } = useFetch();
+  const [delayedLoading, setLoadingDelay] = useState(true);
   const reversedBlogs = blogs ? [...blogs].reverse() : [];
-  if (loading) return <p>Loading...</p>;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadingDelay(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading || delayedLoading)
+    return (
+      <div className="loadingBlogPage flexColumn">
+        <div className="blogEntrySkeleton flex">
+          <div className="blogCover loading"></div>
+          <div className="blogContent loading"></div>
+        </div>
+        <div className="blogEntrySkeleton flex">
+          <div className="blogCover loading"></div>
+          <div className="blogContent loading"></div>
+        </div>
+        <div className="blogEntrySkeleton flex">
+          <div className="blogCover loading"></div>
+          <div className="blogContent loading"></div>
+        </div>
+      </div>
+    );
   if (error) {
     console.log(error);
-    return <p>Error</p>;
+    return <NotFound />;
   }
   // console.log(blogs);
   // console.log(reversedBlogs);
